991a4d75b62329646221c0322c75caf33b4b365e
wanggaowan <wanggaowan@163.com>
2025-04-02 10:37:41 +0800

    修复从git提交记录中记录日志时，在主线程直接调用触发EDT阻塞错误Bug

--- src/main/kotlin/com/wanggaowan/tools/actions/log/SaveGitCommitLogAction.kt
+++ src/main/kotlin/com/wanggaowan/tools/actions/log/SaveGitCommitLogAction.kt
@@ -4,6 +4,7 @@ import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.openapi.vcs.ui.CommitMessage
@@ -54,26 +55,28 @@ class SaveGitCommitLogAction : DumbAwareAction() {
                 return
             }
 
-            val hashesMap: MutableMap<VirtualFile, MutableList<String>> = mutableMapOf()
-            commits.forEach {
-                var list = hashesMap[it.root]
-                if (list == null) {
-                    list = mutableListOf()
-                    hashesMap[it.root] = list
-                }
-                list.add(it.hash.asString())
-            }
-
-            hashesMap.forEach {
-                val provider = logProviders[it.key] ?: return@forEach
-                try {
-                    provider.readMetadata(it.key, it.value) { logData ->
-                        val message = logData.fullMessage
-                        LogUtils.save(projectName, message)
+            ApplicationManager.getApplication().executeOnPooledThread {
+                val hashesMap: MutableMap<VirtualFile, MutableList<String>> = mutableMapOf()
+                commits.forEach {
+                    var list = hashesMap[it.root]
+                    if (list == null) {
+                        list = mutableListOf()
+                        hashesMap[it.root] = list
                     }
-                } catch (_: Exception) {
-                    NotificationUtils.showBalloonMsg(project, "日志记录过程发生异常", NotificationType.ERROR)
-                    return
+                    list.add(it.hash.asString())
+                }
+
+                hashesMap.forEach {
+                    val provider = logProviders[it.key] ?: return@forEach
+                    try {
+                        provider.readMetadata(it.key, it.value) { logData ->
+                            val message = logData.fullMessage
+                            LogUtils.save(projectName, message)
+                        }
+                    } catch (_: Exception) {
+                        NotificationUtils.showBalloonMsg(project, "日志记录过程发生异常", NotificationType.ERROR)
+                        return@forEach
+                    }
                 }
             }
         }
